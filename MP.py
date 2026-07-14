91152d11808a454367f2f8be5ceb7955f47cbf25
an-vi-rus <andrey.yu.vinogradov@icloud.com>
2026-07-14 18:59:03 +0300

    Added fantasy support

--- MP.py
+++ MP.py
@@ -1,3 +1,4 @@
+
 from concurrent.futures import ProcessPoolExecutor as executor
 import tkinter
 from field import Field, Player, init_data
@@ -5,6 +6,8 @@ from field import Field, Player, init_data
 
 root = tkinter.Tk()
 init_data(root)
-field_ = Field()
-players = [Player(0), Player(1), Player(2)]
+#field = Field()
+#players = [Player(0), Player(1), Player(2)]
+
+
 root.mainloop()
--- field.py
+++ field.py
@@ -1,25 +1,23 @@
+import sys
 import tkinter as tk
-from tkinter import ttk, IntVar, Radiobutton
+from tkinter import ttk, IntVar, Radiobutton, Button
 from PIL import Image, ImageTk
 
-
-
-# test
 def init_data(root_g):
-    global w, h, canvas, root, rb_value, screen_width, screen_height
+    global w, h, canvas, root, rb_value, screen_width, screen_height, field
     root = root_g
     root.title("Ananas")
-    root.attributes("-fullscreen", True)
-    root.update_idletasks()
+    if sys.platform == "darwin": root.attributes("-fullscreen", True)
+    else: root.state("zoomed")
     root.update_idletasks()
     screen_width = root.winfo_width()
     screen_height = root.winfo_height()
     rb_value = IntVar()
     rb_value.set(0)
+    root.bind('<Key>', key_pressed)
     h = int(screen_height / 12)
     w = int(h / 1.4)
- 
-
+    field = Field()
 def set_card_imgs():
     global card_imgs
     card_imgs = []
@@ -32,7 +30,9 @@ def set_card_imgs():
             with Image.open(filename) as f:
                 img = ImageTk.PhotoImage(f.resize((w - 2, h - 2), 2))
                 card_imgs.append(img)
-
+def key_pressed(event):
+    match event.keysym:
+        case 'f': rb_value.set(9)
 class Card:
     def __init__(self, n):
         self.n = n
@@ -46,23 +46,34 @@ class Card:
             if rb_value.get() < 9:
                 player = players[rb_value.get() // 3]
                 row = rb_value.get() % 3
-                if player.free_cells[row]: 
+                if player.cells[row]: 
                     player.append_card(self, row)
-                    self.state = 'placed'
-                    return
+                return
+            else:
+                fantasy.append_card(self)
+                return
         if self.state == 'placed':
             for player in players:
                 for i in range(3):
                     if self.n in player.rows[i]:
                         player.remove_card(self, i)
                         return
+        if self.state == 'selected':
+            if rb_value.get() // 3 == 2:
+                if players[2].cells[rb_value.get() % 3]:
+                    players[2].append_card(self, rb_value.get() % 3)
+                    fantasy.remove_card(self.n)
+            else:
+                fantasy.remove_card(self.n)
+                self.reset()
+
+                    
     def reset(self):
         self.state = 'free'
         canvas.coords(self.id, (self.x, self.y))
-
 class Field:
     def __init__(self):
-        global canvas, players, cards
+        global canvas, players, cards, fantasy
         frame = ttk.Frame(root)
         frame.pack(fill='both', expand=True)
         canvas = tk.Canvas(frame, bg='lightgreen', bd=0)
@@ -71,40 +82,77 @@ class Field:
         cards = []
         for i in range(52): cards.append(Card(i))
         players = [Player(0), Player(1), Player(2)]
-
+        fantasy = Fantasy()
 class Player:
     def __init__(self, n):
         self.n = n
-        self.x = 50 + 800 * (n % 2) + 420 * (n // 2)
-        self.y = 300 + 300 * (n // 2)
+        match n:
+            case 0:
+                self.x = 2 * w
+                self.y = screen_height - 9 * h
+            case 1: 
+                self.x = screen_width - 6 * w
+                self.y = screen_height - 9 * h
+            case 2: 
+                self.x = screen_width // 2 - 2.5 * w
+                self.y = screen_height - 3.5 * h
         self.x1, self.y1 = [self.x + 1.5*w, self.x + 0.5*w, self.x + 0.5*w], [self.y + 0.5*h, self.y + 1.5*h, self.y + 2.5*h]
         self.rows = [[], [], []]
         self.combos = [(0, []), (0, []), (0, [])]
-        self.free_cells = [3, 5, 5]
+        self.cells = [3, 5, 5]
         rb_labels = ['High', 'Mid', 'Low']
         for c in range(1,4): canvas.create_rectangle(self.x + w*c, self.y, self.x+w*(c+1), self.y + h)
         for c in range(0,5): canvas.create_rectangle(self.x + w*c, self.y+h, self.x+w*(c+1), self.y + h*2)
         for c in range(0,5): canvas.create_rectangle(self.x + w*c, self.y+h*2, self.x+w*(c+1), self.y + h*3)
         for i in range(3): 
-            rb = Radiobutton(text=rb_labels[i], variable=rb_value, value=3*self.n+i, indicatoron=False)
+            rb = Radiobutton(text=rb_labels[i], variable=rb_value, value=3*self.n+i, indicatoron=False, font=("TkinterDefaultFont", 10))
             canvas.create_window(self.x-w, self.y+h*i, window=rb, width=w-5, height=h, anchor='nw')
     def reset(self):
         self.rows = [[], [], []]
-        self.free_cells = [3, 5, 5]
+        self.cells = [3, 5, 5]
     def append_card(self, card: Card, row):
         shift = 0.5 if row == 0 else -0.5
         self.rows[row].append(card.n)
         canvas.coords(card.id, (self.x+w*(len(self.rows[row])+shift),self.y+h*(row+0.5)))
-        self.free_cells[row] -= 1
+        self.cells[row] -= 1
+        card.state = 'placed'
     def remove_card(self, card: Card, row):
         self.rows[row].remove(card.n)
-        self.free_cells[row] += 1
+        self.cells[row] += 1
         card.reset()
         self.sort_cards(row)
     def sort_cards(self, row):
         shift = 1.5 if row == 0 else 0.5
         for i in range(len(self.rows[row])):
             canvas.coords(cards[self.rows[row][i]].id, (self.x+w*(i+shift)),self.y+h*(row+0.5))
-
-
-     
+class Fantasy:
+    def __init__(self):
+        self.point = (screen_width // 2 - 7 * w, screen_height - 5 * h)
+        self.radiobutton = Radiobutton(text = 'Fantasy', variable=rb_value, value=9, bg='lightgreen', font=('TkDefaultFont, 11'))
+        self.next_button = Button(text = 'Next', bg='lightgreen', command=self.next, font=('TkDefaultFont, 11'))
+        canvas.create_window(self.point[0] - 2 * w, self.point[1] + h, window=self.radiobutton, anchor='sw')
+        canvas.create_window(self.point[0] - 1.5 * w, self.point[1], window=self.next_button, anchor='nw')
+        self.reset()
+    def reset(self):
+        self.cards = []
+        self.dropped = []
+        self.step = 0
+        rb_value.set(9)
+    def append_card(self, card: Card):
+        canvas.coords(card.id, (self.point[0]+w*(len(self.cards)+0.5),self.point[1]+h*0.5))
+        self.cards.append(card.n)
+        card.state = 'selected'     
+    def sort_cards(self):
+        for i in range(len(self.cards)):
+            canvas.coords(cards[self.cards[i]].id, (self.point[0]+w*(i+0.5)),self.point[1]+h*0.5)
+    def drop_card(self, card: Card):
+        card.state = 'dropped'
+        self.cards.remove(card.n)
+        self.dropped.append(card.n)
+        canvas.coords(card.id, (self.point[0]+w*((len(self.dropped)+12)),self.point[1]+h*0.5))
+        self.sort_cards()
+    def remove_card(self, card: Card):
+        self.cards.remove(card)
+        self.sort_cards()
+    def next(self):
+        pass
